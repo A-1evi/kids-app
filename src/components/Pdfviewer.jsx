4edd405f2c1ace05bd34a0755df570031e48f0f4
Alankar <alankar010499@gmail.com>
2024-09-24 18:29:46 +0530

    change path of import

--- src/components/Pdfviewer.jsx
+++ src/components/Pdfviewer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Viewer, Worker } from "@react-pdf-viewer/core";
-import packageJson from "D:/kids-app/package.json";
+import packageJson from "/kids-app/package.json";
 
 // Import the styles
 import "@react-pdf-viewer/core/lib/styles/index.css";
