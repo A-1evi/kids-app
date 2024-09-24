1b5a51da09626be2b3221b2b9166fb41732b053e
Alankar <alankar010499@gmail.com>
2024-09-24 14:43:45 +0530

    rectangle

--- src/components/Shapes.jsx
+++ src/components/Shapes.jsx
@@ -13,16 +13,20 @@ const shapes = [
     name: "Triangle",
     src: "https://ik.imagekit.io/tceymjb1w/kids-app/images.png?updatedAt=1727167790057",
   },
+  {
+    name: "Rectangle",
+    src: "https://ik.imagekit.io/tceymjb1w/kids-app/vector-yellow-rectangle-character-cute-geometric-figure-with-face-blush-hair-cute-funny-smiling-shape-character-kids-children-design-rectangle-poster-school-kindergarten_616756-1818%20(1).avif?updatedAt=1727167790251",
+  },
 ];
 
 const ShapesPage = () => {
   return (
-    <div className="shapes-page">
+    <div className="shapes-page h-screen  bg-gradient-to-b from-white to-fuchsia-300">
       <h1 className="text-2xl font-bold mb-6">Shapes Gallery</h1>
       <div className="grid grid-cols-3 gap-4">
         {shapes.map((shape, index) => (
           <div key={index} className="shape-item flex flex-col items-center">
-            <img src={shape.src} alt={shape.name} className="w-32 h-32" />
+            <img src={shape.src} alt={shape.name} className="w-52 h-52 mix-blend-multiply" />
             <p className="mt-2 text-lg">{shape.name}</p>
           </div>
         ))}
